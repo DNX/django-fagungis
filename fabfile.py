6414a98023ab2992a395093305e4c3e691ef3bc0
Darii Denis <denis.darii@gmail.com>
2011-11-23 17:05:26 +0100

    added install_requirements, create_virtualenv and install_virtualenv

--- fabfile.py
+++ fabfile.py
@@ -11,7 +11,7 @@ from fabric.colors import _wrap_with
 green_bg = _wrap_with('42')
 red_bg = _wrap_with('41')
 
-REPOSITORY = 'ssh://hg@bitbucket.org/DNX/project'
+REPOSITORY = 'https://bitbucket.org/DNX/django-fagungis'
 PROVIDING_PROJECT = ('prod', )
 
 
@@ -25,8 +25,10 @@ def prod():
     env.django_user = 'django'
     #  the code of your project will be located here
     env.django_user_home = join('/opt', env.django_user)
+    #  projects path
+    env.projects_path = join(env.django_user_home, 'projects')
     #  the root path of your project
-    env.code_root = join(env.django_user_home, 'projects', env.project)
+    env.code_root = join(env.projects_path, env.project)
     #  the path where manage.py and settings.py of this project is located
     env.django_project_root = join(env.code_root, 'sites', 'prod')
     #  django media dir
@@ -35,6 +37,8 @@ def prod():
     env.django_static_path = join(env.code_root, 'static')
     #  virtualenv root
     env.virtenv = join(env.django_user_home, 'envs', env.project)
+    #  some virtualenv options, must have at least one
+    env.virtenv_options = ['distribute', 'no-site-packages', ]
     #  always ask user for confirmation when run any tasks
     env.ask_confirmation = True
 
@@ -56,8 +60,31 @@ def _create_django_user():
         sudo('passwd %(django_user)s' % env)
 
 
+def install_dependencies():
+    ''' Ensure those Debian/Ubuntu packages are installed '''
+    packages = [
+        'mercurial',
+        'python-pip',
+    ]
+    sudo('aptitude install %s' % ' '.join(packages))
+
+
+def _install_requirements():
+    ''' you must have a file called requirements.txt in your project root'''
+    requirements = join(env.code_root, 'requirements.txt')
+    virtenvrun('pip install -r %s' % requirements)
+
+
+def _install_virtualenv():
+    sudo('pip install virtualenv')
+
+
+def _create_virtualenv():
+    sudo('virtualenv --%s %s' % (' --'.join(env.virtenv_options), env.virtenv))
+
+
 def _setup_directories():
-    sudo('mkdir -p %(code_root)s' % env)
+    sudo('mkdir -p %(projects_path)s' % env)
     sudo('mkdir -p %(django_user_home)s/logs/nginx' % env)
     sudo('mkdir -p %(django_user_home)s/logs/projects' % env)
     sudo('mkdir -p %(django_user_home)s/configs/nginx' % env)
@@ -65,13 +92,28 @@ def _setup_directories():
     sudo('mkdir -p %(django_user_home)s/scripts' % env)
     sudo('mkdir -p %(django_user_home)s/htdocs' % env)
     sudo('mkdir -p %(django_user_home)s/tmp' % env)
+    sudo('mkdir -p %(virtenv)s' % env)
     sudo('echo "<html><body>nothing here</body></html> " > %(django_user_home)s/htdocs/index.html' % env)
+    sudo('chown -R %(django_user)s %(django_user_home)s' % env)
 
 
-def pull():
+def hg_pull():
     require('hosts', provided_by=PROVIDING_PROJECT)
     with cd(env.code_root):
         run('hg pull -u')
+
+
+def virtenvrun(command):
+    activate = 'source %s/bin/activate' % env.virtenv
+    run(activate + ' && ' + command)
+
+
+def _hg_clone():
+    with settings(hide('running', 'stdout', 'stderr', 'warnings'), warn_only=True):
+        res = run('hg clone %s %s' % (REPOSITORY, env.code_root))
+        if 'is not empty' in res:
+            abort("Code root is not empty. Aborting setup.")
+    puts('%s correctly cloned.' % REPOSITORY)
 
 
 def setup():
@@ -85,8 +127,13 @@ def setup():
     start_time = datetime.now()
 
     sudo('cd .')  # do nothing, just authenticate sudo user
+    install_dependencies()
     _create_django_user()
     _setup_directories()
+    _hg_clone()
+    _install_virtualenv()
+    _create_virtualenv()
+    _install_requirements()
 
     end_time = datetime.now()
     finish_message = '[%s] Correctly finished in %i seconds' % \
