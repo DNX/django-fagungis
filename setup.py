c8b018f51f5e3244ea1698eeb55e5d3b3311059f
Darii Denis <denis.darii@gmail.com>
2011-11-25 18:07:20 +0100

    added setup.py file, we are ready to go in pypi.python.org

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import os
+import fagungis
+
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: Console',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'Intended Audience :: System Administrators',
+    'License :: BSD License',
+    'Operating System :: MacOS :: MacOS X',
+    'Operating System :: Unix',
+    'Operating System :: POSIX',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.5',
+    'Programming Language :: Python :: 2.6',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Build Tools',
+    'Topic :: Software Development :: Libraries',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: System :: Clustering',
+    'Topic :: System :: Software Distribution',
+    'Topic :: System :: Systems Administration',
+]
+
+setup(
+    name="django-fagungis",
+    version=fagungis.__version__,
+    url='https://bitbucket.org/DNX/django-fagungis/wiki/Home',
+    download_url='http://bitbucket.org/DNX/django-fagungis/downloads',
+    license='BSD License',
+    description="DJANGO + FAbric + GUnicorn + NGInx + Supervisor deployment",
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    author='Darii Denis',
+    author_email='info@ddenis.com',
+    keywords='django fabric gunicorn nginx supervisor',
+    packages=find_packages(),
+    namespace_packages=['fagungis'],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=CLASSIFIERS,
+    install_requires=[
+        'Fabric>=1.3',
+    ],
+)
