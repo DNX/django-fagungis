02c336e7a0fd3119dfc410fb66db708223ee8471
Darii Denis <denis.darii@gmail.com>
2011-11-30 11:58:43 +0100

    added verbose functionality to test_configuration task

--- fagungis/tasks.py
+++ fagungis/tasks.py
@@ -5,7 +5,7 @@ from fabric.operations import sudo, settings, run
 from fabric.contrib import console
 from fabric.contrib.files import upload_template
 
-from fabric.colors import _wrap_with
+from fabric.colors import _wrap_with, green
 
 
 green_bg = _wrap_with('42')
@@ -20,7 +20,7 @@ fagungis_path = dirname(abspath(__file__))
 
 @task
 def setup():
-    if not test_configuration():
+    if not test_configuration(False):
         if not console.confirm("Configuration test %s! Do you want to continue?" % red_bg('failed'), default=False):
             abort("Aborting at user request.")
     if env.ask_confirmation:
@@ -50,7 +50,7 @@ def setup():
 
 @task
 def deploy():
-    if not test_configuration():
+    if not test_configuration(False):
         if not console.confirm("Configuration test %s! Do you want to continue?" % red_bg('failed'), default=False):
             abort("Aborting at user request.")
     _verify_sudo()
@@ -81,60 +81,114 @@ def hg_pull():
 
 
 @task
-def test_configuration():
+def test_configuration(verbose=True):
     errors = []
+    parameters_info = []
+    import pdb; pdb.set_trace()
     if 'project' not in env or not env.project:
         errors.append('Project name missing')
+    elif verbose:
+        parameters_info.append(('Project name', env.project))
     if 'repository' not in env or not env.repository:
         errors.append('Repository url missing')
+    elif verbose:
+        parameters_info.append(('Repository url', env.repository))
     if 'hosts' not in env or not env.hosts:
         errors.append('Hosts configuration missing')
+    elif verbose:
+        parameters_info.append(('Hosts', env.hosts))
     if 'django_user' not in env or not env.django_user:
         errors.append('Django user missing')
+    elif verbose:
+        parameters_info.append(('Django user', env.django_user))
     if 'django_user_group' not in env or not env.django_user_group:
         errors.append('Django user group missing')
+    elif verbose:
+        parameters_info.append(('Django user group', env.django_user_group))
     if 'django_user_home' not in env or not env.django_user_home:
         errors.append('Django user home dir missing')
+    elif verbose:
+        parameters_info.append(('Django user home dir', env.django_user_home))
     if 'projects_path' not in env or not env.projects_path:
-        errors.append('Project path configuration missing')
+        errors.append('Projects path configuration missing')
+    elif verbose:
+        parameters_info.append(('Projects path', env.projects_path))
     if 'code_root' not in env or not env.code_root:
         errors.append('Code root configuration missing')
+    elif verbose:
+        parameters_info.append(('Code root', env.code_root))
     if 'django_project_root' not in env or not env.django_project_root:
         errors.append('Django project root configuration missing')
+    elif verbose:
+        parameters_info.append(('Django project root', env.django_project_root))
     if 'django_media_path' not in env or not env.django_media_path:
         errors.append('Django media path configuration missing')
+    elif verbose:
+        parameters_info.append(('Django media path', env.django_media_path))
     if 'django_static_path' not in env or not env.django_static_path:
         errors.append('Django static path configuration missing')
+    elif verbose:
+        parameters_info.append(('Django static path', env.django_static_path))
     if 'south_used' not in env:
         errors.append('"south_used" configuration missing')
+    elif verbose:
+        parameters_info.append(('south_used', env.south_used))
     if 'virtenv' not in env or not env.virtenv:
         errors.append('virtenv configuration missing')
+    elif verbose:
+        parameters_info.append(('virtenv', env.virtenv))
     if 'virtenv_options' not in env or not env.virtenv_options:
         errors.append('"virtenv_options" configuration missing, you must have at least one option')
+    elif verbose:
+        parameters_info.append(('virtenv_options', env.virtenv_options))
     if 'ask_confirmation' not in env:
         errors.append('"ask_confirmation" configuration missing')
+    elif verbose:
+        parameters_info.append(('ask_confirmation', env.ask_confirmation))
     if 'gunicorn_bind' not in env or not env.gunicorn_bind:
         errors.append('"gunicorn_bind" configuration missing')
+    elif verbose:
+        parameters_info.append(('gunicorn_bind', env.gunicorn_bind))
     if 'gunicorn_logfile' not in env or not env.gunicorn_logfile:
         errors.append('"gunicorn_logfile" configuration missing')
+    elif verbose:
+        parameters_info.append(('gunicorn_logfile', env.gunicorn_logfile))
     if 'gunicorn_workers' not in env or not env.gunicorn_workers:
         errors.append('"gunicorn_workers" configuration missing, you must have at least one worker')
+    elif verbose:
+        parameters_info.append(('gunicorn_workers', env.gunicorn_workers))
     if 'gunicorn_worker_class' not in env or not env.gunicorn_worker_class:
         errors.append('"gunicorn_worker_class" configuration missing')
+    elif verbose:
+        parameters_info.append(('gunicorn_worker_class', env.gunicorn_worker_class))
     if 'gunicorn_loglevel' not in env or not env.gunicorn_loglevel:
         errors.append('"gunicorn_loglevel" configuration missing')
+    elif verbose:
+        parameters_info.append(('gunicorn_loglevel', env.gunicorn_loglevel))
     if 'nginx_server_name' not in env or not env.nginx_server_name:
         errors.append('"nginx_server_name" configuration missing')
+    elif verbose:
+        parameters_info.append(('nginx_server_name', env.nginx_server_name))
     if 'supervisorctl' not in env or not env.supervisorctl:
         errors.append('"supervisorctl" configuration missing')
+    elif verbose:
+        parameters_info.append(('supervisorctl', env.supervisorctl))
     if 'supervisor_autostart' not in env or not env.supervisor_autostart:
         errors.append('"supervisor_autostart" configuration missing')
+    elif verbose:
+        parameters_info.append(('supervisor_autostart', env.supervisor_autostart))
     if 'supervisor_autorestart' not in env or not env.supervisor_autorestart:
         errors.append('"supervisor_autorestart" configuration missing')
+    elif verbose:
+        parameters_info.append(('supervisor_autorestart', env.supervisor_autorestart))
     if 'supervisor_redirect_stderr' not in env or not env.supervisor_redirect_stderr:
         errors.append('"supervisor_redirect_stderr" configuration missing')
+    elif verbose:
+        parameters_info.append(('supervisor_redirect_stderr', env.supervisor_redirect_stderr))
     if 'supervisor_stdout_logfile' not in env or not env.supervisor_stdout_logfile:
         errors.append('"supervisor_stdout_logfile" configuration missing')
+    elif verbose:
+        parameters_info.append(('supervisor_stdout_logfile', env.supervisor_stdout_logfile))
 
     if errors:
         if len(errors) == 26:
@@ -146,6 +200,9 @@ def test_configuration():
             puts('-' * 40)
             puts('Please fix them or go ahead at your own risk.')
         return False
+    elif verbose:
+        for parameter in parameters_info:
+            puts('%s: %s' % (parameter[0], green(parameter[1])))
     puts('Configuration tests passed!')
     return True
 
